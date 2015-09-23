901ef1e849960f39b5049311f7308b69aadbe15a
Andy Shora <Andy.Shora@rga.com>
2015-09-23 11:38:33 +0100

    Working custom element

---
+++ src/components/peg/peg.js
@@ -0,0 +1,9 @@
+import {bindable} from 'aurelia-framework';
+
+@bindable({ name: 'number' })
+@bindable({ name: 'word' })
+export class Peg {
+  constructor() {}
+
+  bind() {}
+}
--- src/welcome.js
+++ src/welcome.js
@@ -1,10 +1,13 @@
 //import {computedFrom} from 'aurelia-framework';
+import Peg from './components/peg/peg';
 
 export class Welcome{
   heading = 'Welcome to the Aurelia Navigation App!';
   firstName = 'John';
   lastName = 'Doe';
   previousValue = this.fullName;
+
+  pegs = [{ number: 1, word: 'test' }, { number: 2, word: 'test' }, { number: 3, word: 'test' }]
 
   //Getters can't be directly observed, so they must be dirty checked.
   //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
